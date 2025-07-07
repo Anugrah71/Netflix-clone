0bc81bc70de219df773f804be24b63622baa820b
Anugrah71 <your-email@example.com>
2025-07-07 16:22:31 +0530

    Thride commit

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
-import './TitleCards.css'
-export const TitleCards = () => {
+import React, { useRef, useEffect } from "react";
+import "./TitleCards.css";
+import cards_data from "../../assets/cards/Cards_data";
+export const TitleCards = ({title , category}) => {
+
+  const cardsRef = useRef();
+  const handlewheel = (e) => {
+    e.preventDefault();
+    cardsRef.current.scrollLeft += e.deltaY;
+  };
+  useEffect(() => {
+    cardsRef.current.addEventListener("wheel", handlewheel);
+  }, []);
+
   return (
-    <div className="titlecards">TitleCards</div>
-  )
-}
+    <div className="title-cards">
+      <h2>{title?title:"Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardsRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
+};
 export default TitleCards;
